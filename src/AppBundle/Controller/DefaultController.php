e3530d195b775074b2802bfaba4f8809a995309b
MounirOnGithub <m.khanouri@live.fr>
2016-08-22 12:31:25 +0200

    routing

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -1,21 +1,20 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: Mounir
+ * Date: 22/08/2016
+ * Time: 12:23
+ */
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
-    public function indexAction(Request $request)
-    {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
+    public function helloAction(){
+
+        return $this->render("AppBundle::index.html.twig");
     }
 }
